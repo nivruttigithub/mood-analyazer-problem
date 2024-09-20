a7656f11f246cbe741a4c2be5c66b719c6750697
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-20 10:58:46 +0530

    Added : Mood Analyzer

---
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.moodanalyzer;
+
+public class MoodAnalyzer {
+    public String analyseMood(String message) {
+
+        if (message.contains("Sad")) {
+            return "Sad";
+        } else {
+            return "Happy";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+
+        String message = "I am feeling so sad today!";
+        String mood = moodAnalyser.analyseMood(message);
+
+        System.out.println("Mood: " + mood);
+    }
+}
